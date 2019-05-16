3c2cec9c088c6d126c64248e70115fbf19a33f4a
Samuel Boateng <kaybeta500@gmail.com>
2019-05-16 06:37:15 +0000

    server.js changes

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
-const
+/* const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
@@ -9,5 +9,12 @@ const app = express()
 
 app.use(history())
 app.use(serveStatic(path.join(__dirname, '/pwa-mat')))
-console.log(path.join(__dirname, '/pwa-mat'))
-app.listen(port)
+app.listen(port) */
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname, '/pwa-mat'));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
