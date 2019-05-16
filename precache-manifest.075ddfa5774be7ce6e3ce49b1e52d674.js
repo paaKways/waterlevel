495ace2df3df90ea33630517b5b088c00a17b0d0
Samuel Boateng <kaybeta500@gmail.com>
2019-05-16 06:57:30 +0000

    new dist

--- precache-manifest.075ddfa5774be7ce6e3ce49b1e52d674.js
+++ precache-manifest.075ddfa5774be7ce6e3ce49b1e52d674.js
@@ -21,15 +21,15 @@ self.__precacheManifest = [
   },
   {
     "revision": "3020c8ac2c2872dec7741e5948520093",
-    "url": "statics/icons/icon-128x128.png"
+    "url": "statics/quasar-logo.png"
   },
   {
     "revision": "d4b280b4bd1b1915043f",
     "url": "js/app.2fef1a2a.js"
   },
   {
-    "revision": "3020c8ac2c2872dec7741e5948520093",
-    "url": "statics/quasar-logo.png"
+    "revision": "66d7719a8ca03c1a591444c013802817",
+    "url": "statics/icons/icon-384x384.png"
   },
   {
     "revision": "5896d4fd26d70c5b4ae9",
@@ -40,32 +40,28 @@ self.__precacheManifest = [
     "url": "js/vendor.5e350034.js"
   },
   {
-    "revision": "66d7719a8ca03c1a591444c013802817",
-    "url": "statics/icons/icon-384x384.png"
-  },
-  {
     "revision": "a13b2bd5261e2df374128df1aab93001",
     "url": "statics/icons/icon-512x512.png"
-  },
-  {
-    "revision": "ed12d10fd0407eea42ccaf7af7991146",
-    "url": "statics/icons/ms-icon-144x144.png"
   },
   {
     "revision": "20c6420ccf16a07d9d56538e30a6b524",
     "url": "statics/icons/icon-256x256.png"
   },
   {
+    "revision": "ed12d10fd0407eea42ccaf7af7991146",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
     "revision": "8c4120b4e88276badbc808e518572fad",
     "url": "statics/icons/icon-192x192.png"
   },
   {
-    "revision": "76b776bc87e97e8ab9f1",
-    "url": "js/32a5c660.3c19b250.js"
-  },
-  {
     "revision": "da2b422e2895b9b9c1782a71b6be9d63",
     "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "76b776bc87e97e8ab9f1",
+    "url": "js/32a5c660.3c19b250.js"
   },
   {
     "revision": "2f9d7587924933c1912c342b190b53ca",
@@ -76,8 +72,12 @@ self.__precacheManifest = [
     "url": "statics/icons/favicon-16x16.png"
   },
   {
-    "revision": "de8b7431b74642e830af4d4f4b513ec9",
-    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
   },
   {
     "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
@@ -100,8 +100,8 @@ self.__precacheManifest = [
     "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
   },
   {
-    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
-    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
   },
   {
     "revision": "5896d4fd26d70c5b4ae9",
